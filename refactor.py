225ae1c06c64ebbe2f0139d512f7f13d639432a9
Lucas <lucasfiorini@msn.com>
2019-09-12 01:14:37 -0300

    :hammer: Refactor

--- refactor.py
+++ refactor.py
@@ -1,30 +1,33 @@
 import ast
 
-def imprimeRelatorio(dup_list):
+
+def imprime_relatorio(lista_duplicatas):
     while dup_list:
-        first = dup_list.pop()
+        first = lista_duplicatas.pop()
         print("Metodo " + first.nome + " repetido nas classes: ")
-        print("\t"  + first.classe)
+        print("\t" + first.classe)
 
         for noh in dup_list:
             if noh.nome == first.nome:
                 print("\t" + noh.classe)
-                dup_list.remove(noh)
+                lista_duplicatas.remove(noh)
 
-def separaDuplicatas(metodos_classes, dup_list):
-    while metodos_classes:
-        c = metodos_classes.pop()
+
+def separa_duplicatas(lista_metodos_classes, lista_duplicatas):
+    while lista_metodos_classes:
+        c = lista_metodos_classes.pop()
         seen = False
-        for met in metodos_classes:
+        for met in lista_metodos_classes:
             if met.nome == c.nome:
-                dup_list.append(met)
-                metodos_classes.remove(met)
+                lista_duplicatas.append(met)
+                lista_metodos_classes.remove(met)
                 seen = True
         if seen:
-            dup_list.append(c)
+            lista_duplicatas.append(c)
 
-def percorreArvore(arvore, metodos_classes):
-    for noh in ast.walk(arvore):
+
+def percorre_arvore(arvore_gerada, lista_metodos_classes):
+    for noh in ast.walk(arvore_gerada):
         if not isinstance(noh, ast.ClassDef):
             continue
         classe = noh
@@ -36,19 +39,19 @@ def percorreArvore(arvore, metodos_classes):
                         # Ignorando construtor
                         if nome_metodo != "__init__":
                             c = InfoMetodo(classe.name, nome_metodo)
-                            metodos_classes.append(c)
+                            lista_metodos_classes.append(c)
                     else:
                         c = InfoMetodo(classe.name, nome_metodo)
-                        metodos_classes.append(c)
+                        lista_metodos_classes.append(c)
 
 
 class InfoMetodo():
     nome = " "
     classe = " "
 
-    def __init__(self, nomeClasse, nomeMetodo):
-        self.nome = nomeMetodo
-        self.classe = nomeClasse
+    def __init__(self, nome_classe, nome_metodo):
+        self.nome = nome_metodo
+        self.classe = nome_classe
 
 
 with open("Exemplo.py", "r") as codigo_analisado:
@@ -57,9 +60,9 @@ with open("Exemplo.py", "r") as codigo_analisado:
 metodos_classes = []
 
 
-percorreArvore(arvore, metodos_classes)
+percorre_arvore(arvore, metodos_classes)
 dup_list = []
 
-separaDuplicatas(metodos_classes, dup_list)
+separa_duplicatas(metodos_classes, dup_list)
 
-imprimeRelatorio(dup_list)
+imprime_relatorio(dup_list)
