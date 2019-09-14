bbee5775d25b6031704a4f5bfb633e567efd53af
Felipe Palmuti <fepalmuti@gmail.com>
2019-09-14 02:18:03 -0300

    Funcionalidade de subir os metodos implementada

---
+++ src/Exemplo.py
@@ -0,0 +1,25 @@
+class Animal():
+    def __init__(self, numero_patas):
+        pass
+
+
+class Cachorro(Animal):
+    def __init__(self, numero_patas):
+        pass
+
+    def andar():
+        pass
+
+    def comer():
+        pass
+
+
+class Gato(Animal):
+    def __init__(self, numero_patas):
+        pass
+
+    def andar():
+        pass
+
+    def comer():
+        pass
