c2672839508f6903002c43ce0ced80e04b125b91
Lucas <lucasfiorini@msn.com>
2019-10-19 19:06:15 -0300

    :construction:

---
+++ Similaridade/Metrica.py
@@ -0,0 +1,3 @@
+class Metrica:
+    pass
+# TODO atribuir pesos para cada tipo de dado que tenha na estrutura talvez
---
+++ Similaridade/View.py
@@ -0,0 +1,4 @@
+class View:
+    pass
+# TODO interface com o usuario
+# caso possivel, comparar uma classe com outra, uma classe com todas as outras e todas entre si
