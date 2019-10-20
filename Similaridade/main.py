10514e3db0a62b954ba25cd51a05ce4a54bdeac8
fepalmuti <fepalmuti@gmail.com>
2019-10-20 20:34:41 -0300

    Logica integrada com a geracao do relatorio das metricas

--- Similaridade/main.py
+++ Similaridade/main.py
@@ -1,9 +1,11 @@
 from Similaridade import Similaridade
+from Coefficients import Statistics
 
 def main():
     s = Similaridade("exemplo.py")
     a, b, c, d = s.calcular_a_b_c_d("Teste", "Util")
-    print(a, b, c, d)
+    st = Statistics()
+    st.generateValues(a, b, c, d)
 
 
 if __name__ == '__main__':
