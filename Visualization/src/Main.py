d2dd8ccfd55bd282e6a891ce5707c972336c14ce
Felipe Palmuti <fepalmuti@gmail.com>
2019-12-02 22:22:49 -0300

    Adicao dos nomes das classes e de cores diferentes

--- Visualization/src/Main.py
+++ Visualization/src/Main.py
@@ -1,11 +1,15 @@
 from MontagemDeClasses import *
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
 
 def plotar(lista_classes, largura, espacamento):
     profundidades = []
     alturas = []
     lista_coord_x = []
+    labels = []
+    xs = []
     x = 0
     #---------------------
     fig = plt.figure()
@@ -15,6 +19,7 @@ def plotar(lista_classes, largura, espacamento):
         profundidades.append(classe["qnt_atributos"])
         alturas.append(classe["qnt_metodos"])
         lista_coord_x.append(x)
+        labels.append(classe["nome"])
         x += espacamento
     #---------------------
     xpos = lista_coord_x
@@ -23,9 +28,19 @@ def plotar(lista_classes, largura, espacamento):
     dx = len(lista_classes) * [largura]
     dy = profundidades
     dz = alturas
-    ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color='#00ceaa')
+
+    xlabels = np.array(labels)
+
+    ax1.w_xaxis.set_ticks(xpos)
+    ax1.w_xaxis.set_ticklabels(xlabels)
+    colors = cm.rainbow(np.linspace(0,1,len(ypos)))
+
+    ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors, alpha = 0.5)
+
+    ax1.set_xlabel("Nome das Classes")
     ax1.set_ylabel("Qnt. Atributos")
     ax1.set_zlabel("Qnt. Metodos")
+
     plt.show()
 
 lista_classes = montar_classes("Exemplo.py")
